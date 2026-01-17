dc4f02f6f79fd6f53925e51ff760b585429fcc75
Lain Soykaf <lain@lain.com>
2026-01-17 12:32:10 +0400

    ConfigController: Don't allow updating the whitelist
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- test/pleroma/web/admin_api/controllers/config_controller_test.exs
+++ test/pleroma/web/admin_api/controllers/config_controller_test.exs
@@ -20,6 +20,33 @@ defmodule Pleroma.Web.AdminAPI.ConfigControllerTest do
     {:ok, %{admin: admin, token: token, conn: conn}}
   end
 
+  describe "POST /api/pleroma/admin/config" do
+    test "doesn't allow updating the database_config_whitelist itself", %{conn: conn} do
+      original_whitelist = Pleroma.Config.get(:database_config_whitelist)
+
+      refute ConfigDB.get_by_group_and_key(:pleroma, :database_config_whitelist)
+
+      conn =
+        conn
+        |> put_req_header("content-type", "application/json")
+        |> post("/api/pleroma/admin/config", %{
+          configs: [
+            %{
+              group: ":pleroma",
+              key: ":database_config_whitelist",
+              value: [%{"tuple" => [":pleroma", ":key1"]}]
+            }
+          ]
+        })
+
+      %{"configs" => configs} = json_response_and_validate_schema(conn, 200)
+
+      assert configs == []
+      assert Pleroma.Config.get(:database_config_whitelist) == original_whitelist
+      refute ConfigDB.get_by_group_and_key(:pleroma, :database_config_whitelist)
+    end
+  end
+
   describe "GET /api/pleroma/admin/config/descriptions" do
     test "all keys from description are whitelisted", %{conn: conn} do
       conn = get(conn, "/api/pleroma/admin/config/descriptions")
