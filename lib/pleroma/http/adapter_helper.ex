external-mirrors/akkoma

commit ed5d609ba4edbbddaaad935f8a2728be0157ac68

http: do not add adapter pool options to individual requests
They do nothing. As documented[1] only three specific
options regarding timeouts are parsed for individual request
and none of them is set by AdapterHelper, only pool-specific options.

In particular this means we always relied on Mint’s default CA cert
verification based on queries to the CAStore package (which we include).

[1]: https://hexdocs.pm/finch/Finch.html#request/3-options

lib/pleroma/http/adapter_helper.ex
@@ -15,12 +15,6 @@ defmodule Pleroma.HTTP.AdapterHelper do
 
   @type proxy :: {Connection.proxy_type(), Connection.host(), pos_integer(), list()}
 
-  def maybe_add_cacerts(opts, nil), do: opts
-
-  def maybe_add_cacerts(opts, cacerts) do
-    put_in(opts, [:pools, :default, :conn_opts, :transport_opts, :cacerts], cacerts)
-  end
-
   @doc """
   Merge default connection & adapter options with received ones.
   """

lib/pleroma/http.ex
@@ -7,7 +7,6 @@ defmodule Pleroma.HTTP do
     Wrapper for `Tesla.request/2`.
   """
 
-  alias Pleroma.HTTP.AdapterHelper
   alias Tesla.Env
 
   require Logger
@@ -58,17 +57,6 @@ defmodule Pleroma.HTTP do
   @spec request(method(), Request.url(), String.t(), Request.headers(), keyword()) ::
           {:ok, Env.t()} | {:error, any()}
   def request(method, url, body, headers, options) when is_binary(url) do
-    uri = URI.parse(url)
-    adapter_opts = AdapterHelper.options(options[:adapter] || [])
-
-    adapter_opts =
-      if uri.scheme == :https do
-        AdapterHelper.maybe_add_cacerts(adapter_opts, :public_key.cacerts_get())
-      else
-        adapter_opts
-      end
-
-    options = put_in(options[:adapter], adapter_opts)
     params = options[:params] || []
     options = options |> Keyword.delete(:params)
     headers = maybe_add_user_agent(headers)

commit be21f914f40450ef3d2928fa40368b05338bf621

mix: bump finch and use system cacerts
This upgrade pulls in a fix to better avoid killing re-actived pools,
obsoletes the need for our own HTTP2 server push workaround and allows
us to use system CA certs without breaking plain HTTP connections.

We tried to to the latter before on a per request basis, but this didn’t
actually do anything and we actually relied on the CAStore package
fallback the entire time. The broken attempt was removed in
ed5d609ba4edbbddaaad935f8a2728be0157ac68.

Resolves: https://akkoma.dev/AkkomaGang/akkoma/issues/880

lib/pleroma/http/adapter_helper.ex
@@ -29,13 +29,11 @@ defmodule Pleroma.HTTP.AdapterHelper do
           conn_max_idle_time: Config.get!([:http, :receive_timeout]),
           protocols: Config.get!([:http, :protocols]),
           conn_opts: [
-            # Do NOT add cacerts here as this will cause issues for plain HTTP connections!
-            # (when we upgrade our deps to Mint >= 1.6.0 we can also explicitly enable "inet4: true")
-            transport_opts: [inet6: true],
-            # up to at least version 0.20.0, Finch leaves server_push enabled by default for HTTP2,
-            # but will actually raise an exception when receiving such a response. Tell servers we don't want it.
-            # see: https://github.com/sneako/finch/issues/325
-            client_settings: [enable_push: false]
+            # When we upgrade our deps to Mint >= 1.6.0 we can also explicitly enable "inet4: true"
+            transport_opts: [
+              inet6: true,
+              cacerts: :public_key.cacerts_get()
+            ]
           ]
         ]
       }

mix.exs
@@ -146,7 +146,7 @@ defmodule Pleroma.Mixfile do
       {:tesla, "~> 1.16.0"},
       {:castore, "~> 1.0"},
       {:cowlib, "~> 2.12"},
-      {:finch, "~> 0.20.0"},
+      {:finch, "~> 0.21.0"},
       {:jason, "~> 1.4"},
       {:trailing_format_plug, "~> 0.0.7"},
       {:mogrify, "~> 0.9"},