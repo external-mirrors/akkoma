a14d405e784dd51aa619feb49fa1925c20f81870
nicole mikołajczyk <git@mkljczk.pl>
2026-03-06 18:50:50 +0100

    Update default config
    Signed-off-by: nicole mikołajczyk <git@mkljczk.pl>

--- config/config.exs
+++ config/config.exs
@@ -571,7 +571,6 @@ config :pleroma, :database_config_whitelist, [
   {:cors_plug},
   {:ex_aws, :s3},
   {:mime},
-  {:prometheus, Pleroma.Web.Endpoint.MetricsExporter},
   {:web_push_encryption, :vapid_details},
   {:logger}
 ]
